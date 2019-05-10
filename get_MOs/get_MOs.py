ebf46deb53663d8676453f5f827cafa84c354227
Mithik <jakub.lang@gmail.com>
2019-05-10 10:53:26 +0200

    upgrade

--- get_MOs/get_MOs.py
+++ get_MOs/get_MOs.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import math
 import sys
+import re
 from collections import deque
 
 ################ MOLECULE INFO CLASS ###########################
+# Some basic info about system from ORCA's output. Mainly good to 
+# determine Fermi vacuum
 class MolInfo:
     def __init__ (self,n_elec=0,mult=0,dim=0,found_seg=False):
         self.n_elec = (int)(n_elec)
@@ -18,12 +21,16 @@ class MolInfo:
         print ("")
 
 ################ base MO CLASS #########################
+# Base class, used for loewdin reduced MO analysis.
+# Stores only orbitals with orb content higher than use specified threshold
 class MO(object):
-    def __init__ (self,header=None,pop=None,orb_number=-1,en=0):
+    def __init__ (self,header=None,pop=None,orb_number=-1,en=0,isBeta=False):
         self.header = header
-        self.pop    = [float(i) for i in pop]
+        if pop is not None:
+            self.pop    = [float(i) for i in pop]
         self.orb_n  = (int)(orb_number)
         self.energy = (float)(en)
+        self.isBeta = isBeta
 
         if all(len(i) != len(self.header[0]) for i in self.header):
             header_str = ""
@@ -35,23 +42,31 @@ class MO(object):
             print("There is some problem with creating MO. Different sizes for lists;  header:" +  str(len(self.header[0])) + " C: " + str(len(self.pop))) 
             exit()
 
-    def printMO (self):   
+    def printMO (self,orb_atom,orb_type):   
         thres = 5
         if self.orb_n == -1:
             print("Problem with MO number")
             exit()
-        print ("Printing orbital E(\033[91mv{:3d}v\033[0m): {:9.6f}".format(self.orb_n,self.energy))
+        print ("Printing orbital E({:3d}): {:9.6f}".format(self.orb_n,self.energy))
         for i in range(0,len(self.pop)):
             if abs(self.pop[i]) > thres:
-                header_str = ""
+                if self.isBeta is True:
+                    header_str = "Beta "
+                else:
+                    header_str = ""
                 for item in self.header:
                     header_str+=item[i]+" "
-
-                print ("{:1.10s} \033[92m {:4.1f} \033[0m %".format(header_str,self.pop[i]))
+                if orb_atom in header_str and orb_type in header_str:
+                    print ("{:1.10s} \033[92m{:4.1f}\033[0m %".format(header_str,self.pop[i]))
+                else:
+                    print ("{:1.10s} {:4.1f} %".format(header_str,self.pop[i]))
         print("")
 
 
 ################ AO_MO CLASS #########################
+# Derived class from MO, stores AO_MO cooeficients higher
+# than hardcored value, default is 0.00001
+# Basically same as base class
 class AO_MO(MO):
 
     def printMO (self):   
@@ -59,28 +74,54 @@ class AO_MO(MO):
         if self.orb_n == -1:
             print("Problem with MO number")
             exit()
-        print ("Printing orbital E(\033[91m {:3d} \033[0m): {:9.6f}".format(self.orb_n,self.energy))
+        print ("Printing orbital E({:3d}): \033[91m{:9.6f}\033[0m ".format(self.orb_n,self.energy))
         for i in range(0,len(self.pop)):
             if abs(self.pop[i]) > thres:
-                header_str = ""
+                if self.isBeta is True:
+                    header_str = "Beta "
+                else:
+                    header_str = ""
                 for item in self.header:
                     header_str+=item[i]+" "
 
-                #print (header_str + " " + '\033[92m' + str(self.pop[i]) +  '\033[0m')
-                print ("{:<10} \033[92m {:<12.6} \033[0m".format(header_str,self.pop[i]))
+                print (header_str + " " + '\033[92m' + str(self.pop[i]) +  '\033[0m')
+                #print ("{:<10} {:<12.6}".format(header_str,self.pop[i]))
         print("")
-    def orb_content (self, orb_name):
-        orb_sum = 0
-        total   = 0
+
+    def orb_content (self, orb_name, orb_atom, thres):
+        orb_sum  = []
+        total    = 0
+        print_it = False 
         for i in range(0,len(self.pop)):
             total += self.pop[i]*self.pop[i]
-            if orb_name in self.header[1][i]:
-                orb_sum += self.pop[i]*self.pop[i]
-        ratio = orb_sum/total*100
-        if ratio > 10:
-            print ("Orbital {} {} content is {:4.2f}".format(self.orb_n,orb_name,ratio))
-        return ratio
-# Auxialary functions 
+            orb_sum.append(self.pop[i]*self.pop[i])
+        
+        ratio = [x/total*100 for x in orb_sum]
+        for i in range(0,len(ratio)):
+            if orb_name in self.header[1][i] and orb_atom in self.header[0][i] and ratio[i] > thres:
+                print_it = True
+        if print_it is True:
+            print ("Printing orbital E({:3d}): {:9.6f} ".format(self.orb_n,self.energy))
+            for i in range(0,len(ratio)):
+                if ratio[i] > 5:
+                    tmp = re.split('(\D+)',self.header[0][i])
+                    if self.isBeta is True:
+                        header_str = "\033[91m "
+                    else:
+                        header_str = " "
+                    header_str = header_str + tmp[0] + " " + tmp[1] + " " + self.header[1][i]
+                    if orb_atom in header_str and orb_type in header_str:
+                        print ("{:1.10s} \033[92m{:4.1f}\033[0m %".format(header_str,ratio[i]))
+                    else:
+                        print ("{:1.10s} {:4.1f}\033[0m %".format(header_str,ratio[i]))
+                    #print ("Orbital {self.heade} {} content is {:4.2f}".format(self.orb_n,orb_name,ratio))
+        if print_it is True:
+            if self.orb_n is None:
+                print ("THE FUCK IS WRONG WITH YOU!!!!")
+            return self.orb_n
+
+#Find two empty lines after matrix print, is same in every ORCA output,
+# unless they will break it in future
 def empty_lines(empty,line):
     empty_line   = False
     empty_2lines = False
@@ -93,16 +134,22 @@ def empty_lines(empty,line):
             empty_2lines = False
     return empty_line,empty_2lines
     
+# Nice pythonic way to compare two lists :)
 def compare_lists(list1,list2):
     return list(set(list2)-set(list1))
 
-def find_permutations(list1,list2):
-    compare_lists(list1,list2)
-    return a
+#def find_permutations(list1,list2):
+#    compare_lists(list1,list2)
+#    return a
 
 
 #Fuck Python
 def parseOutput(filename, atom, orb_type, thrs,thrsAOMO):
+# Bunch of local variables and switches
+# It works in such way, that it always stores last three lines
+# in the memory. This is because coeeficient and data starts with
+# a pattern ----- ---- etc. and orbital numbers are written out
+# in 3 lines earlier
     seg_header   = deque(maxlen=3)
     empty_line  = False
     empty_2lines = False
@@ -118,7 +165,7 @@ def parseOutput(filename, atom, orb_type, thrs,thrsAOMO):
     n_elec       = 0
     mult         = 0
     dim          = 0
-
+    isBeta       = False
     while line:
         if "FINAL SINGLE POINT ENERGY" in line:
             break
@@ -146,32 +193,42 @@ def parseOutput(filename, atom, orb_type, thrs,thrsAOMO):
            read_seg = list()
            save_MOs = False
            #while "--------  --------  --------  --------  --------  --------" not in line and line:
+           # Find where orbital coeffs starts
            while "                  --------  " not in line and line :
                empty_line,empty_2lines=empty_lines(empty_line,line)
                if empty_2lines is True:
                    break
                if "LOEWDIN REDUCED ORBITAL POPULATIONS PER MO" in line:
                    break
+               #Obviously ORCA is stupid and sometimes there is not space between two coeffs
                data = line.split()
+               coeffs = re.findall(r'[0-9]+.[0-9]{6}',line)
+               data = data[0:2]
+               data.extend(coeffs)
                read_seg.append(data)
                if len(data) > 0 and atom in data[0] and orb_type in data[1]:
                    for i in range(2,len(data)):
                        if (float)(data[i]) > thrsAOMO:
                            save_MOs = True
-                           orb_list.append([orb_num[i-3],data[0],data[1],data[i]])
+                           # Save orbital number, atom, orb_type and coefficients
+                           orb_list.append([orb_num[i-2],data[0],data[1],data[i]])
+                           # also save an absolute column index
                            idx2save.add(i)
                seg_header.append(line)
                line = inputfile.readline()
-
+           # ORCA prints in six columns blocks, here we take care of distinguishing between
+           # last or not last block
            if save_MOs is True:
                if empty_2lines is True:
                    read_seg = read_seg[:len(read_seg)-1]
+                   isBeta = True
                else:
                    read_seg = read_seg[:len(read_seg)-3]
                read_seg = list(map(list,zip(*read_seg)))
                MO_header = [read_seg[0],read_seg[1]]
                for i in idx2save:
-                  MOs_AO_MO.append(AO_MO(MO_header,read_seg[i],orb_num[i-2],orb_en[i-2]))
+                  # create instance for every MO above threshold 
+                  MOs_AO_MO.append(AO_MO(MO_header,read_seg[i],orb_num[i-2],orb_en[i-2],isBeta))
 
            orb_num = seg_header[0].split()
            orb_en  = seg_header[1].split()
@@ -179,11 +236,13 @@ def parseOutput(filename, atom, orb_type, thrs,thrsAOMO):
         ######################################
         # Parse Loewdin segment
         ######################################
+        # Same things as in previous section
         if "LOEWDIN REDUCED ORBITAL POPULATIONS PER MO" in line:
            print ("Starting to analyze the Loewdin population per MO")
            found_pop = True
            #reset lists and bools
            found_mos = False
+           isBeta    = False
            orb_list.clear()
         
         if "MAYER POPULATION ANALYSIS" in line:
@@ -211,19 +270,20 @@ def parseOutput(filename, atom, orb_type, thrs,thrsAOMO):
            if save_MOs is True:
                if empty_2lines is True:
                     read_seg = read_seg[:len(read_seg)-1]
+                    isBeta = True
                else:
                     read_seg = read_seg[:len(read_seg)-5]
                read_seg = list(map(list,zip(*read_seg)))
                MO_header = [read_seg[0],read_seg[1],read_seg[2]]
 
                for i in idx2save:
-                  MOs_pop.append(MO(MO_header,read_seg[i],orb_num[i-3],orb_en[i-3]))
+                  MOs_pop.append(MO(MO_header,read_seg[i],orb_num[i-3],orb_en[i-3],isBeta))
 
            orb_num = seg_header[0].split()
            orb_en  = seg_header[1].split()
            read_seg.clear() 
         line = inputfile.readline()
-
+    inputfile.close()
     # sort according to MO number
     orb_list.sort(key=lambda x: int(x[0]))
     MOs_pop.sort(key=lambda x: x.orb_n)
@@ -234,6 +294,7 @@ def parseOutput(filename, atom, orb_type, thrs,thrsAOMO):
     
     return orb_list,mol_info,MOs_pop,MOs_AO_MO
 
+# creates orbitals for charmol, needs it in 1...99999 format
 def prepareCharmolOrbs(orb_list,mol_info):
     orbs = []
     for entry in orb_list:
@@ -248,7 +309,7 @@ def prepareCharmolOrbs(orb_list,mol_info):
     max_occ      = (int)(mol_info.n_elec/2+mol_info.mult%2)
     iorb         = 0
     for iorb in range(0,len(orbs)):
-        if (int)(orbs[iorb]) < max_occ:
+        if (int)(orbs[iorb]) < max_occ-1:
             charmol_occ+=str(int(orbs[iorb])+1)+" "
         else:
             break
@@ -284,14 +345,24 @@ prepareCharmolOrbs(orbitals,mol_info)
 mol_info.printInfo()
 
 for orb in MOs:
-    orb.printMO()
+    orb.printMO(atom,orb_type)
 
-print ("You should also considerate these orbitals")
-extra_MOs=compare_lists([item.orb_n for item in MOs], [item.orb_n for item in AOMOs])
-for n in extra_MOs:
-    orb=next((item for item in MOs if n == item.orb_n),None)
-    if orb is not None:
-        orb.printMO()
-    orb=next((item for item in AOMOs if n == item.orb_n),None)
-    if orb is not None:
-        orb.orb_content('d')
+mos_in_AOMOs = []
+print ("Printing all orbitals with content of " + orb_type + " higher than " + str(thrs/2))
+for orb in AOMOs:
+    #orb.printMO()
+    #this is stupid way to do it, there will be a lot of 'None's but I am too lazy to do it correctly 
+    mos_in_AOMOs.append(orb.orb_content(orb_type,atom,thrs/2))
+print ("Orbitals not common for both methods")
+extra_MOs=compare_lists([item.orb_n for item in MOs], [item for item in mos_in_AOMOs if item is not None]) # here goes the bug fix for None
+extra_MOs.extend(compare_lists([item for item in mos_in_AOMOs if item is not None], [item.orb_n for item in MOs]))
+extra_MOs.sort(key=lambda x: int(x))
+print (extra_MOs)
+#for n in extra_MOs:
+#    orb=next((item for item in MOs if n == item.orb_n),None)
+#    if orb is not None:
+#        orb.printMO()
+#    orb=next((item for item in AOMOs if n == item.orb_n),None)
+#    if orb is not None:
+#        orb.orb_content('d')
+
